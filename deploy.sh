0bd8c6b4891448d41e24811eb0c6b21a191618e6
yuliuxia <yuliuxia@baidu.com>
2020-11-04 11:01:53 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -2,21 +2,9 @@
 
 # 提交md文档
 git checkout main
-# git pull
-# git add .
-# git commit -m "修改文档"
-# git push origin main
-# git checkout master # git checkout -b master origin master
-# git merge main
-
+git pull
 #构建
 npm run docs:build
-
-rm -rf ./asserts
-cp -rf docs/.vuepress/dist/ ./
-rm -rf docs/.vuepress/dist
-
 git add .
 git commit -m 'deploy'
-git push origin master
-git checkout main
+git push origin main
